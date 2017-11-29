70ae38266aa390f96edc3998e2f6d07291697549
Junior <juniorquegu@gmail.com>
2017-11-28 22:16:21 -0500

    First commit

---
+++ app/src/main/java/com/cjava/example/MainActivity.java
@@ -0,0 +1,20 @@
+package com.cjava.example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_view);
+
+
+
+    }
+
+
+
+
+}
