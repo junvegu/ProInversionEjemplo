26cf91a773a66eb42cdb04ecf430525123ed6121
Junior <juniorquegu@gmail.com>
2017-11-29 21:06:07 -0500

    Add Intents

--- app/src/main/java/com/cjava/example/MainActivity.java
+++ app/src/main/java/com/cjava/example/MainActivity.java
@@ -1,9 +1,19 @@
 package com.cjava.example;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    private Button mButtonLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,9 +22,54 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+    }
+
+    private Context getContext(){
+        return this;
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
     }
 
 
 
 
+
+
+
+
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+    }
 }
---
+++ app/src/main/java/com/cjava/example/SecondActivity.java
@@ -0,0 +1,31 @@
+package com.cjava.example;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by junior on 29/11/17.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+
+
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+
+}
