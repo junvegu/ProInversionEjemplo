2a3ffd46582d7bbf15bb13c3f540aaf72b81add1
Junior <juniorquegu@gmail.com>
2017-12-05 21:44:04 -0500

    Camera

--- app/src/main/java/com/cjava/example/presentation/contacts/ActivityListContacts.java
+++ app/src/main/java/com/cjava/example/presentation/contacts/ActivityListContacts.java
@@ -1,6 +1,8 @@
 package com.cjava.example.presentation.contacts;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
@@ -9,16 +11,20 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.widget.Toast;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.cjava.example.R;
 import com.cjava.example.data.local.SessionManager;
 import com.cjava.example.model.ContacsModel;
 import com.cjava.example.presentation.contacts.adapter.ContactsAdapter;
 import com.cjava.example.presentation.login.LoginActivity;
+import com.cjava.example.utils.ImagePicker;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by junior on 30/11/17.
@@ -31,6 +37,12 @@ public class ActivityListContacts extends AppCompatActivity {
     RecyclerView rvContacts;
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.image)
+    ImageView image;
+    @BindView(R.id.btn_select_image)
+    Button btnSelectImage;
+
+    private static final int PICK_IMAGE_ID = 1234;
 
 
     private ContactsAdapter mContactsAdapter;
@@ -55,7 +67,7 @@ public class ActivityListContacts extends AppCompatActivity {
         ab.setTitle("Lista de contactos");
 
 
-        sessionManager =  new SessionManager(this);
+        sessionManager = new SessionManager(this);
 
         mContactsAdapter = new ContactsAdapter(ContacsModel.contacsEntities(), this);
         mLinearLayoutManager = new LinearLayoutManager(this);
@@ -66,12 +78,13 @@ public class ActivityListContacts extends AppCompatActivity {
         rvContacts.setLayoutManager(mLinearLayoutManager);
 
     }
+
     @Override
     public boolean onSupportNavigateUp() {
 
 
         sessionManager.closeSession();
-        Intent intent =  new Intent(this, LoginActivity.class);
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         finish();
 
@@ -80,4 +93,32 @@ public class ActivityListContacts extends AppCompatActivity {
     }
 
 
+    @OnClick(R.id.btn_select_image)
+    public void onViewClicked() {
+
+        Intent chooseImageIntent = ImagePicker.getPickImageIntent(this);
+        startActivityForResult(chooseImageIntent, PICK_IMAGE_ID);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode) {
+                case PICK_IMAGE_ID:
+
+                            Bitmap bitmap1 = ImagePicker.getImageFromResult(this, resultCode, data);
+                            image.setImageBitmap(bitmap1);
+
+                    break;
+                default:
+
+                    break;
+            }
+        }
+
+
+    }
 }
